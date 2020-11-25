4df9926810a68edeedaab4870faaa5864eb47766
newbiehe1 <956389219@qq.com>
2020-11-25 16:43:28 +0800

    新增webpack打包日志输出

--- webpack/build.js
+++ webpack/build.js
@@ -7,7 +7,8 @@ const ProgressBarPlugin = require("progress-bar-webpack-plugin");
 
 config = merge(config, {
     mode: "production",
-    // devtool: "inline-cheap-source-map",
+    devtool: "inline-cheap-source-map",
+
     optimization: {
         minimizer: [
             new TerserPlugin({
@@ -25,19 +26,22 @@ config = merge(config, {
             cleanStaleWebpackAssets: false,
         }),
         new ProgressBarPlugin({
-            format: "",
-            clear: true,
             summary: false,
         }),
     ],
-    cache: false,
 });
 
 process.stderr.write(` build start ..... \n\n`);
+
 // module.exports = config;
 
-webpack(config, (err, state) => {
+webpack(config, (err, stats) => {
     // console.log("##########################################################");
     // console.log(state);
-    // console.log(state);
+    console.log(
+        stats.toString({
+            chunks: false, // 使构建过程更静默无输出
+            colors: true, // 在控制台展示颜色
+        })
+    );
 });
